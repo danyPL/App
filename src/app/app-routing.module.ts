09c051d42f0a40579861220f995ff4d498405887
dany <renatasiwiec71@gmail.com>
2023-11-28 14:34:09 +0100

    Pliki

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+import { UsersComponent } from './users/users.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    title:"Home",
+    component:HomeComponent,
+    path:''
+  },
+  {
+    title:"Blog",
+    component:BlogComponent,
+    path:'blog'
+  },
+  {
+    title:"Users",
+    component:UsersComponent,
+    path:'users'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
