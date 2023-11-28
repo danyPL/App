3a7a42e78baae03b8b3e3ecd361d8adec2b5780e
dany <renatasiwiec71@gmail.com>
2023-11-28 14:55:18 +0100

    do a flip

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild,ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  @ViewChild("imie") imie?: ElementRef;
+  @ViewChild("lista") lista?:ElementRef;
+  constructor(private render:Renderer2){
 
+  }
+getData(){
+let elementlist = this.render.createElement("li");
+this.render.addClass(elementlist,"list-group-item");
+elementlist.innerText=this.imie?.nativeElement.value;
+this.render.appendChild(this.lista?.nativeElement,elementlist);
+
+}
+showAlert(){
+  alert("to jest alert")
+  
+}
 }
